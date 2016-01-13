9c06c3fa12a3b5f1743ff8c77435b4826f15aa40
samjarzembowski <sjarzembowski@gmail.com>
2016-01-12 16:05:20 -0800

    Reflects around three hours of work from Sawako and Sam.

---
+++ class-07-functional-programming/sawako-sam/starter-code/scripts/article.js
@@ -0,0 +1,102 @@
+// TODO: Wrap the entire contents of this file in an IIFE.
+// Pass in to the IIFE a module, upon which objects can be attached for later access.
+(function(module) {
+  function Article (opts) {
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
+  }
+
+  Article.all = [];
+  // authorArray = [];
+
+  Article.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#article-template').text());
+
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
+
+    return template(this);
+  };
+
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    // DONE: Refactor this forEach code, by using a `.map` call instead, since want we are trying to accomplish
+    // is the transformation of one colleciton into another.
+    // rawData.forEach(function(ele) {
+    //   Article.all.push(new Article(ele));
+    // })
+    Article.all = rawData.map(function(ele) {
+      return new Article(ele);
+    });
+  };
+
+  // This function will retrieve the data from either a local or remote source,
+  // and process it, then hand off control to the View.
+  // TODO: Refactor this function, so it accepts an argument of a callback function (likely a view function)
+  // to execute once the loading of articles is done.
+  Article.fetchAll = function(foo) {
+
+    if (localStorage.rawData) {
+      Article.loadAll(JSON.parse(localStorage.rawData));
+      // Article.fetchAll(articleView.initIndexPage);
+      console.log('testing fetchall ');
+    } else {
+      $.getJSON('/data/hackerIpsum.json', function(rawData) {
+        Article.loadAll(rawData);
+        localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
+        // Article.fetchAll(articleView.initIndexPage);
+      });
+    }
+  };
+  Article.fetchAll();
+  // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  Article.numWordsAll = function() {
+    console.log('hit numWordsAll')
+    return Article.all.map(function(article) {
+      // console.log('map each article')
+      var words = article.body.split(' ');  //split the article-body into a new array STRIP HTML
+      return words.length; // Currently Approximate Get the total number of words in this article
+    })
+    .reduce(function(a, b) {
+      return (a + b);  // Sum up all the values in the collection
+    });
+
+  }; console.log('I am here')
+  Article.numWordsAll();
+  // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
+  Article.allAuthors = function() {
+    return Article.all.map(function(article) {
+      return article.author;
+    })
+      .reduce(function(names, name){
+        if(names.indexOf(name) === -1){
+          names.push(name)
+        }
+        return names;
+        console.log(names);
+      },[]);
+  };
+  Article.allAuthors();
+
+
+
+
+  Article.numWordsByAuthor = function() {
+    // TODO: Transform each author string into an object with 2 properties: One for
+    // the author's name, and one for the total number of words across all articles written by the specified author.
+    return Article.allAuthors().map(function(author) {
+      return {
+        // someKey: someValOrFunctionCall().map(...).reduce(...), ...
+      }
+    })
+  };
+  module.Article = Article;
+})(window);
